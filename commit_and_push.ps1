821358186ae187ef81e57e2c12bd470e41d8153f
Rafael Sinjunatha Wulangsih <rafaelsinjunta@gmail.com>
2024-03-07 15:19:37 +0700

    edit commit_and_push.ps1

--- commit_and_push.ps1
+++ commit_and_push.ps1
@@ -1,18 +1,32 @@
-# Prompt for the main project commit message
-$main_commit_message = Read-Host -Prompt "Enter your main project commit message"
+# Save the current directory
+$original_path = Get-Location
 
-# Stage, commit, and push changes in the main project
-git add .
-git commit -m $main_commit_message
-git push origin main
+# Check if there are changes in the main project
+if ((git diff --quiet) -ne 0)
+{
+    # Prompt for the main project commit message
+    $main_commit_message = Read-Host -Prompt "Enter your main project commit message"
+
+    # Stage, commit, and push changes in the main project
+    git add .
+    git commit -m $main_commit_message
+    git push origin main
+}
 
 # Navigate to the submodule directory
 Set-Location -Path boilerplates
 
-# Prompt for the submodule commit message
-$submodule_commit_message = Read-Host -Prompt "Enter your submodule commit message"
+# Check if there are changes in the submodule
+if ((git diff --quiet) -ne 0)
+{
+    # Prompt for the submodule commit message
+    $submodule_commit_message = Read-Host -Prompt "Enter your submodule commit message"
 
-# Stage, commit, and push changes in the submodule
-git add .
-git commit -m $submodule_commit_message
-git push origin boilerplates-ta
+    # Stage, commit, and push changes in the submodule
+    git add .
+    git commit -m $submodule_commit_message
+    git push origin boilerplates-ta
+}
+
+# Return to the original directory
+Set-Location -Path $original_path
