c2947305a2f2ce81995bd091f7d832f62f3d2cc0
Rafael Sinjunatha Wulangsih <rafaelsinjunta@gmail.com>
2024-03-11 21:41:48 +0700

    Testing Packer on VirtualBox

--- commit_and_push.ps1
+++ commit_and_push.ps1
@@ -3,7 +3,11 @@ $original_path = Get-Location
 
 # Check if there are changes in the main project
 git diff --quiet
-if ($LASTEXITCODE -ne 0)
+$unstaged_changes = $LASTEXITCODE -ne 0
+git diff --cached --quiet
+$staged_changes = $LASTEXITCODE -ne 0
+
+if ($unstaged_changes -or $staged_changes)
 {
     # Prompt for the main project commit message
     $main_commit_message = Read-Host -Prompt "Enter your main project commit message"
@@ -13,27 +17,17 @@ if ($LASTEXITCODE -ne 0)
     git commit -m $main_commit_message
     git push origin main
 }
-else
-{
-    git diff --cached --quiet
-    if ($LASTEXITCODE -ne 0)
-    {
-        # Prompt for the main project commit message
-        $main_commit_message = Read-Host -Prompt "Enter your main project commit message"
-
-        # Stage, commit, and push changes in the main project
-        git add .
-        git commit -m $main_commit_message
-        git push origin main
-    }
-}
 
 # Navigate to the submodule directory
 Set-Location -Path boilerplates
 
 # Check if there are changes in the submodule
 git diff --quiet
-if ($LASTEXITCODE -ne 0)
+$unstaged_changes = $LASTEXITCODE -ne 0
+git diff --cached --quiet
+$staged_changes = $LASTEXITCODE -ne 0
+
+if ($unstaged_changes -or $staged_changes)
 {
     # Prompt for the submodule commit message
     $submodule_commit_message = Read-Host -Prompt "Enter your submodule commit message"
@@ -43,20 +37,14 @@ if ($LASTEXITCODE -ne 0)
     git commit -m $submodule_commit_message
     git push origin boilerplates-ta
 }
-else
-{
-    git diff --cached --quiet
-    if ($LASTEXITCODE -ne 0)
-    {
-        # Prompt for the submodule commit message
-        $submodule_commit_message = Read-Host -Prompt "Enter your submodule commit message"
 
-        # Stage, commit, and push changes in the submodule
-        git add .
-        git commit -m $submodule_commit_message
-        git push origin boilerplates-ta
-    }
-}
-
-# Return to the original directory
+# Navigate back to the main project directory
 Set-Location -Path $original_path
+
+# Add the updated submodule to the main project
+git add boilerplates
+
+# Commit and push the main project with the updated submodule
+$main_commit_message = Read-Host -Prompt "Enter your main project commit message for updating the submodule"
+git commit -m $main_commit_message
+git push origin main
