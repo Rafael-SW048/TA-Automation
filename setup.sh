67c1973039b464c3093a20cd93eb2c67c1bb6508
Rafael Sinjunatha Wulangsih <rafaelsinjunta@gmail.com>
2024-03-13 20:32:13 +0700

    delete unusable file

--- setup.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update && sudo apt-get upgrade -y
-wget -O- https://apt.releases.hashicorp.com/gpg | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
-echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://apt.releases.hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
-sudo apt update
-
-if ! dpkg-query -W -f='${Status}' packer 2>/dev/null | grep -q "ok installed"; then
-  sudo apt install packer -y
-fi
